13e897f45aac21467c4b1e6440b96aabb2d36d5c
Sma Das <smaddas10@gmail.com>
2022-01-01 00:20:18 +0400

    Update update_front_end.py

--- scripts/update_front_end.py
+++ scripts/update_front_end.py
@@ -2,7 +2,7 @@ import yaml
 import json
 
 
-def update_front_end(brownie_config="./brownie-config.yaml", typescript_src="./front-end-ui/src"):
+def update_front_end(brownie_config="./brownie-config.yaml", typescript_src="./front-end-ui/src/brownie-config.json"):
     with open(brownie_config, "r") as b_config:
         brownie_config_contents = yaml.load(b_config, Loader=yaml.FullLoader)
 
