a3def4e7df207a2cd6c5c3ed076692f1a0780399
Sma Das <smaddas10@gmail.com>
2022-01-10 21:53:02 +0400

    Reformat Main.tsx

--- front-end-ui/src/components/Main.tsx
+++ front-end-ui/src/components/Main.tsx
@@ -1,88 +1,80 @@
-import { useEthers } from "@usedapp/core"
-import deploymentMappings from "../deployments/deployments.json"
-import chainIdMappings from "../deployments/chain_id.json"
-import brownieConfig from "../brownie-config.json"
-import { constants } from "ethers"
+import { useEthers } from "@usedapp/core";
+import deploymentMappings from "../deployments/deployments.json";
+import chainIdMappings from "../deployments/chain_id.json";
+import brownieConfig from "../brownie-config.json";
+import { constants } from "ethers";
 
-import dappImage from "./images/dapp.png"
-import ethImage from "./images/ethereum.png"
-import daiImage from "./images/dai.png"
-import { Wallet } from "./Wallet/Wallet"
-import { makeStyles } from "@material-ui/core"
+import dappImage from "./images/dapp.png";
+import ethImage from "./images/ethereum.png";
+import daiImage from "./images/dai.png";
+import { Wallet } from "./Wallet/Wallet";
+import { makeStyles } from "@material-ui/core";
 
-import logo from "./images/logo.png"
+import logo from "./images/logo.png";
 
 export type Token = {
-    name: string
-    address: string
-    image: string
-}
+  name: string;
+  address: string;
+  image: string;
+};
 
-const useStyles = makeStyles(
-    (theme) => (
-        {
-            title: {
-                color: theme.palette.common.white,
-                textAlign: "center",
-                padding: theme.spacing(4)
-            },
-            tokenImg: {
-                width: "64px",
-                alignItems: "left"
-            }
-        }
-    )
-)
+const useStyles = makeStyles((theme) => ({
+  title: {
+    color: theme.palette.common.white,
+    textAlign: "center",
+    padding: theme.spacing(4),
+  },
+  tokenImg: {
+    width: "64px",
+    alignItems: "left",
+  },
+}));
 
 export const Main = () => {
+  const classes = useStyles();
 
-    const classes = useStyles()
+  const { chainId } = useEthers();
+  const networkName = chainId ? chainIdMappings[String(chainId)] : "N/A";
 
-    const { chainId, error } = useEthers()
-    const networkName = chainId ? chainIdMappings[String(chainId)] : "N/A"
+  const dappTokenAddress = chainId
+    ? deploymentMappings[String(chainId)]["DappToken"][0]
+    : constants.AddressZero;
 
-    console.log(`Chain ID=${chainId ? chainId : "N/A"}`)
-    console.log(`On network=${networkName}`)
+  const wethTokenAddress = chainId
+    ? brownieConfig["networks"][networkName]["weth_token"]
+    : constants.AddressZero;
 
-    const dappTokenAddress = chainId ? deploymentMappings[String(chainId)]["DappToken"][0] : constants.AddressZero
-    console.log(`dappTokenAddress token @${dappTokenAddress}`)
+  const fauTokenAddress = chainId
+    ? brownieConfig["networks"][networkName]["fau_token"]
+    : constants.AddressZero;
 
-    const wethTokenAddress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero
-    console.log(`wethTokenAddress token @${wethTokenAddress}`)
+  const supportedTokens: Array<Token> = [
+    {
+      name: "DAPP",
+      address: dappTokenAddress,
+      image: dappImage,
+    },
+    {
+      name: "ETH",
+      address: wethTokenAddress,
+      image: ethImage,
+    },
+    {
+      name: "DAI",
+      address: fauTokenAddress,
+      image: daiImage,
+    },
+  ];
 
-    const fauTokenAddress = chainId ? brownieConfig["networks"][networkName]["fau_token"] : constants.AddressZero
-    console.log(`fauTokenAddress token @${fauTokenAddress}`)
-
-    const supportedTokens: Array<Token> = [
-        {
-            name: "DAPP",
-            address: dappTokenAddress,
-            image: dappImage
-        },
-        {
-            name: "ETH",
-            address: wethTokenAddress,
-            image: ethImage
-        },
-        {
-            name: "DAI",
-            address: fauTokenAddress,
-            image: daiImage
-        }
-    ]
-
-
-
-    return (
-        <>
-            <div>
-                <h2 className={classes.title}>
-                    <img src={logo} className={classes.tokenImg}></img>
-                    My DaPP Manager!
-                </h2>
-            </div>
-            <Wallet supportedTokens={supportedTokens} />
-        </>
-
-    )
-}
+  return (
+    <>
+      <div>
+        <h2 className={classes.title}>
+          <img src={logo} className={classes.tokenImg}></img>
+          My DaPP Manager!
+        </h2>
+      </div>
+      <Wallet supportedTokens={supportedTokens} />
+    </>
+  );
+};
