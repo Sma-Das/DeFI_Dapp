668caa95ca459595b3598b05d4d8b74b037e7492
Sma Das <smaddas10@gmail.com>
2022-01-10 21:53:20 +0400

    Reformat Balance.tsx, BalanceMsg.tsx

--- front-end-ui/src/components/Wallet/Balance.tsx
+++ front-end-ui/src/components/Wallet/Balance.tsx
@@ -4,23 +4,25 @@ import { Token } from "../Main";
 import { BalanceMsg } from "./BalanceMsg";
 
 export interface BalanceProps {
-    token: Token
+  token: Token;
 }
 
 export const Balance = ({ token }: BalanceProps) => {
-    const { name, address, image } = token
-    console.log(`tokenAddress=${address}`)
-    const { account } = useEthers()
-    console.log(`account=${account}`)
-    const tokenBalance = useTokenBalance(address, account)
-    console.log(`tokenBalance=${tokenBalance}`)
+  const { name, address, image } = token;
+  console.log(`tokenAddress=${address}`);
+  const { account } = useEthers();
+  console.log(`account=${account}`);
+  const tokenBalance = useTokenBalance(address, account);
+  console.log(`tokenBalance=${tokenBalance}`);
 
-    const formattedTokenBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) : 0
-    return (
-        <BalanceMsg
-            label={`Your unstaked ${name} balance`}
-            amount={formattedTokenBalance}
-            tokenImg={image} />
-
-    )
-}
+  const formattedTokenBalance: number = tokenBalance
+    ? parseFloat(formatUnits(tokenBalance, 18))
+    : 0;
+  return (
+    <BalanceMsg
+      label={`Your unstaked ${name} balance`}
+      amount={formattedTokenBalance}
+      tokenImg={image}
+    />
+  );
+};
--- front-end-ui/src/components/Wallet/BalanceMsg.tsx
+++ front-end-ui/src/components/Wallet/BalanceMsg.tsx
@@ -1,42 +1,34 @@
-import { makeStyles } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles(
-    theme => (
-        {
-            container: {
-                display: "inline-grid",
-                gridTemplateColumns: "auto auto auto",
-                gap: theme.spacing(1),
-                alignItems: "center"
-            },
-            tokenImg: {
-                width: "32px"
-            },
-            amount: {
-                fontWeight: 700
-            }
-        }
-    )
-)
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "inline-grid",
+    gridTemplateColumns: "auto auto auto",
+    gap: theme.spacing(1),
+    alignItems: "center",
+  },
+  tokenImg: {
+    width: "32px",
+  },
+  amount: {
+    fontWeight: 700,
+  },
+}));
 
 interface BalanceMsgProps {
-    label: string
-    amount: number
-    tokenImg: string
+  label: string;
+  amount: number;
+  tokenImg: string;
 }
 
 export const BalanceMsg = ({ label, amount, tokenImg }: BalanceMsgProps) => {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    return (
-        <div className={classes.container}>
-            <div>
-                {label}
-            </div>
-            <div className={classes.amount}>
-                {amount}
-            </div>
-            <img className={classes.tokenImg} src={tokenImg} alt="token logo"></img>
-        </div>
-    )
-}
+  return (
+    <div className={classes.container}>
+      <div>{label}</div>
+      <div className={classes.amount}>{amount}</div>
+      <img className={classes.tokenImg} src={tokenImg} alt="token logo"></img>
+    </div>
+  );
+};
